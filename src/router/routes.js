bc9de500eb08f426417b9a30539bbe14067453ab
Kacper Orzechowski <kacper.orzechowski96@gmail.com>
2019-10-02 13:22:47 +0200

    base configuration

---
+++ src/router/routes.js
@@ -0,0 +1,15 @@
+import Home from '@/views/Home'
+import Register from '@/views/Register'
+
+export default [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
+  }
+]
